6c8d038b8cb93d8ad8b33eb896b7404a290f9344
Ondřej Červenka <cerveon3@fit.cvut.cz>
2017-01-15 05:42:16 +0100

    template for cli interface

--- pytRobot/pytRobot.py
+++ pytRobot/pytRobot.py
@@ -1,2 +1,25 @@
+import click
+
+@click.group()
 def main():
-    print('Hello world!!')
+    pass
+
+@main.group()
+def console():
+    pass
+
+@console.command()
+def capture():
+    print('img')
+
+@console.command()
+def turn():
+    print('turn')
+
+@console.command()
+def forward():
+    print('forward')
+
+@main.command()
+def web():
+    print('hello www!!!!')
